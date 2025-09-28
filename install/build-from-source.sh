422361d66eb48f02da3232db375a8dc4d6d65364
VoidArc Studio <chudybyczek2137@gmail.com>
2025-09-28 19:13:08 +0200

    Update build-from-source.sh

--- install/build-from-source.sh
+++ install/build-from-source.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+echo "[INFO] Checking distribution."
 
+echo "[INFO] Checking and installing dependencies."
+
+echo "[RUN] Clones the repository."
+git clone https://github.com/Nula-Lang/Nula.git /tmp/Nula
+cd /tmp/Nula
+
+echo "[RUN] Building nula"
+
+echo "[INFO] The operation has been completed"
+
+echo "[PLEASE] Run the nula command or launch the application from the nula program menu."
