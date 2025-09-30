f05eef98fbdce730443691af5532ec483ae702ed
VoidArc Studio <chudybyczek2137@gmail.com>
2025-09-30 18:35:36 +0200

    Create ast.rs

---
+++ nula/src/ast.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, Clone)]
+pub enum AstNode {
+    Program(Vec<AstNode>),
+    Translation(String, String), // lang, code
+    Dependency(String),
+    Import(String),
+    Comment(String),
+    VariableDecl(String, Box<AstNode>),
+    Assignment(String, Box<AstNode>),
+    FunctionDef(String, Vec<String>, Vec<AstNode>),
+    ForLoop(String, Box<AstNode>, Vec<AstNode>),
+    WhileLoop(Box<AstNode>, Vec<AstNode>),
+    If(Box<AstNode>, Vec<AstNode>, Vec<(Box<AstNode>, Vec<AstNode>)>, Option<Vec<AstNode>>),
+    Write(Box<AstNode>),
+    Add(Box<AstNode>, Box<AstNode>),
+    Mul(Box<AstNode>, Box<AstNode>),
+    Return(Option<Box<AstNode>>),
+    StringLit(String),
+    NumberLit(f64),
+    BoolLit(bool),
+    Ident(String),
+    Call(String, Vec<AstNode>),
+    Binary(Box<AstNode>, String, Box<AstNode>),
+    Unary(String, Box<AstNode>),
+}
