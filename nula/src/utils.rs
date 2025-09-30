aec6838e7d5d99d18fd72c6cd8e5a88393771ee6
VoidArc Studio <chudybyczek2137@gmail.com>
2025-09-30 18:39:15 +0200

    Update utils.rs

--- nula/src/utils.rs
+++ nula/src/utils.rs
@@ -6,8 +6,16 @@ pub fn is_in_project() -> bool {
     Path::new("nula.toml").exists() || Path::new("main.nula").exists()
 }
 
+pub fn get_lib_dir() -> PathBuf {
+    if cfg!(target_os = "windows") {
+        env::var("APPDATA").map(PathBuf::from).unwrap_or_default().join("nula")
+    } else {
+        env::var("HOME").map(PathBuf::from).unwrap_or_default().join(".nula/lib")
+    }
+}
+
 pub fn get_nula_go_path() -> PathBuf {
-    let path = get_nula_bin_path("nula-go");
+    let path = get_lib_dir().join(if cfg!(target_os = "windows") { "nula-go.exe" } else { "nula-go" });
     if !path.exists() {
         print_error(&format!("nula-go not found at {:?}", path));
     }
@@ -15,25 +23,9 @@ pub fn get_nula_go_path() -> PathBuf {
 }
 
 pub fn get_nula_zig_path() -> PathBuf {
-    let path = get_nula_bin_path("nula-zig");
+    let path = get_lib_dir().join(if cfg!(target_os = "windows") { "nula-zig.exe" } else { "nula-zig" });
     if !path.exists() {
         print_error(&format!("nula-zig not found at {:?}", path));
     }
     path
-}
-
-fn get_nula_bin_path(name: &str) -> PathBuf {
-    if cfg!(target_os = "windows") {
-        env::current_exe()
-        .unwrap_or_default()
-        .parent()
-        .unwrap_or(Path::new(""))
-        .join(format!("{}.exe", name))
-    } else {
-        env::current_exe()
-        .unwrap_or_default()
-        .parent()
-        .unwrap_or(Path::new(""))
-        .join(name)
-    }
 }
