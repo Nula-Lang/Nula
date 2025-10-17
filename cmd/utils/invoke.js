cc9a09929208597a92fa4e5851a6683bc9be359e
michal92299 <chudybyczek2137@gmail.com>
2025-10-17 22:23:23 +0200

    Create invoke.js

---
+++ cmd/utils/invoke.js
@@ -0,0 +1,27 @@
+const { execSync } = require('child_process');
+const path = require('path');
+const os = require('os');
+const { getNulaDir } = require('./dirUtils');
+const { error, log } = require('./logger');
+
+module.exports.invokeBinary = function invokeBinary(binaryName, args) {
+  const homeDir = os.homedir();
+  const nulaDir = getNulaDir(homeDir);
+  const binDir = path.join(nulaDir, 'bin');
+  const ext = os.platform() === 'win32' ? '.exe' : '';
+  const binaryPath = path.join(binDir, binaryName + ext);
+  
+  if (!fs.existsSync(binaryPath)) {
+    error(`Binary ${binaryName} not found in ${binDir}`);
+    process.exit(1);
+  }
+  
+  try {
+    execSync(`${binaryPath} ${args.join(' ')}`, { stdio: 'inherit' });
+    log(`Executed ${binaryName} with args: ${args.join(' ')}`);
+  } catch (err) {
+    error(`Error executing ${binaryName}: ${err.message}`);
+    log(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
