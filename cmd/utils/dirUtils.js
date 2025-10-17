13e65c808a5a919294b3f6a102679733cc2feb97
michal92299 <chudybyczek2137@gmail.com>
2025-10-17 22:23:05 +0200

    Create dirUtils.js

---
+++ cmd/utils/dirUtils.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const { error } = require('./logger');
+
+module.exports.getNulaDir = function getNulaDir(homeDir) {
+  return path.join(homeDir, '.nula');
+};
+
+module.exports.ensureDirs = function ensureDirs(nulaDir) {
+  const binDir = path.join(nulaDir, 'bin');
+  const libDir = path.join(nulaDir, 'lib');
+  const logFile = path.join(nulaDir, 'nula.log');
+
+  [nulaDir, binDir, libDir].forEach(dir => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+  });
+
+  if (!fs.existsSync(logFile)) {
+    fs.writeFileSync(logFile, '');
+  }
+};
