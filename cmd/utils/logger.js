7c2265644239a69662a168bfe74d06bf6fa9ddba
michal92299 <chudybyczek2137@gmail.com>
2025-10-18 09:04:01 +0200

    Update logger.js

--- cmd/utils/logger.js
+++ cmd/utils/logger.js
@@ -9,22 +9,27 @@ function getLogFile() {
   return path.join(nulaDir, 'nula.log');
 }
 
-module.exports.log = function log(message) {
+module.exports.log = (message) => {
   const logFile = getLogFile();
   fs.appendFileSync(logFile, `${new Date().toISOString()} - ${message}\n`);
 };
 
-module.exports.error = function error(message) {
-  console.error(chalk.red('Error: ' + message));
-  log('ERROR: ' + message);
+module.exports.error = (message) => {
+  console.error(chalk.red.bold('ERROR: ') + chalk.red(message));
+  this.log('ERROR: ' + message);
 };
 
-module.exports.success = function success(message) {
-  console.log(chalk.green(message));
-  log('SUCCESS: ' + message);
+module.exports.success = (message) => {
+  console.log(chalk.green.bold('SUCCESS: ') + chalk.green(message));
+  this.log('SUCCESS: ' + message);
 };
 
-module.exports.info = function info(message) {
-  console.log(chalk.blue(message));
-  log('INFO: ' + message);
+module.exports.info = (message) => {
+  console.log(chalk.blue.bold('INFO: ') + chalk.blue(message));
+  this.log('INFO: ' + message);
+};
+
+module.exports.warn = (message) => {
+  console.log(chalk.yellow.bold('WARN: ') + chalk.yellow(message));
+  this.log('WARN: ' + message);
 };
